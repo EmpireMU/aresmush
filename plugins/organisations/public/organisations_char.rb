e958811bc4377688daf708c691f751f1976d4fa3
EmpireMU <empiremux@gmail.com>
2026-02-04 21:03:11 +0000

    1

--- plugins/organisations/public/organisations_char.rb
+++ plugins/organisations/public/organisations_char.rb
@@ -8,18 +8,18 @@ module AresMUSH
     
     def in_organisation?(org_name)
       return false if !org_name
-      self.organisations.any? { |o| o.downcase == org_name.downcase }
+      (self.organisations || []).any? { |o| o.downcase == org_name.downcase }
     end
     
     def add_to_organisation(org_name)
       return if self.in_organisation?(org_name)
-      orgs = self.organisations
+      orgs = self.organisations || []
       orgs << org_name
       self.update(organisations: orgs.uniq)
     end
     
     def remove_from_organisation(org_name)
-      orgs = self.organisations.reject { |o| o.downcase == org_name.downcase }
+      orgs = (self.organisations || []).reject { |o| o.downcase == org_name.downcase }
       self.update(organisations: orgs)
     end
   end
